2d114de1a1e4f300d7e63a6520b1d2643492c435
Fasanya Abiola <79338077+fastbeetech@users.noreply.github.com>
2021-06-02 18:47:02 +0100

    Revert "Update main.js"

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ console.clear()
         else{
             const total = `${evaluate}%`
             const color = document.querySelector('.result-container')
-            const pass = color.classList.add('pass').remove('fail')
-            const fail = color.classList.add('fail').remove('pass')
+            const pass = color.classList.add('pass')
+            const fail = color.classList.add('fail')
             let msgs = evaluate > 79 ? "your score is 80% and above" : "Your score is less than 80%"
-            evaluate > 79 ? color.classList.remove('fail').add('pass') : color.classList.remove('pass').add('fail')
+            evaluate > 79 ? pass : fail
             let result = document.getElementById('result').innerHTML = `Your percentage grade is <br> <b>${total}</b> <p>${msgs}<\p>`
             console.log(`${evaluate}%`)
         }
