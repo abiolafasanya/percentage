7df73fce4d95765c6a5fc06c613de476dbe117c6
Fasanya Abiola <79338077+fastbeetech@users.noreply.github.com>
2021-06-02 19:11:18 +0100

    Update main.js

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ console.clear()
         else{
             const total = `${evaluate}%`
             const color = document.querySelector('.result-container')
-            const pass = color.classList.add('pass')
-            const fail = color.classList.add('fail')
+            
+            evaluate > 79 ? color.classList.add('pass') : color.classList.add('fail')
             let msgs = evaluate > 79 ? "your score is 80% and above" : "Your score is less than 80%"
-            evaluate > 79 ? pass : fail
+            
             let result = document.getElementById('result').innerHTML = `Your percentage grade is <br> <b>${total}</b> <p>${msgs}<\p>`
             console.log(`${evaluate}%`)
         }
